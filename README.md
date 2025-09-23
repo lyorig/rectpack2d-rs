180b09c25e971fe5c0e8115d36721b6fc3383e66
Petr Šácha <petrik.sacha@gmail.com>
2025-09-23 11:36:02 +0200

    find_best_packing_font_sort

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 This is an attempt at a functionally 1:1 Rust port of the amazing [rectpack2D](https://github.com/TeamHypersomnia/rectpack2D) library. The motivation behind this is that it's used in a larger C++ project I'm rewriting in Rust, and as I failed to find a "proper" rewrite (only libraries "inspired" by it), I decided to undertake the effort myself.
 
-**Some functionality is missing**, namely:
-- rect flipping of any kind
-- custom orders in `find_best_packing`
+## Implementation checklist
+- [x] basic (`rect_xywh`) functionality
+- [ ] flipping (`rect_xywhf`) functionality
+- [x] ability to provide custom orders
 
 I haven't yet figured out a "best practice" way to implement these, but if you're knowledgeable in the langauge, a PR would be much appreciated.
--- src/finders_interface.rs
+++ src/finders_interface.rs
@@ -29,6 +29,33 @@ impl<F: Fn(RectXYWH) -> CallbackResult, G: Fn(RectXYWH) -> CallbackResult> Input
     }
 }
 
+pub fn find_best_packing_dont_sort<
+    EmptySpacesType: EmptySpacesProviderTrait,
+    T,
+    F: Fn(RectXYWH) -> CallbackResult,
+    G: Fn(RectXYWH) -> CallbackResult,
+>(
+    root: &mut EmptySpaces<EmptySpacesType>,
+    subjects: &mut [T],
+    input: &Input<F, G>,
+) -> RectWH
+where
+    for<'a> &'a mut T: Into<&'a mut RectXYWH>,
+{
+    find_best_packing_impl(
+        root,
+        subjects.iter_mut().map(|i| i.into() as _).collect(),
+        subjects.len(),
+        input,
+    )
+}
+
+/// Forwards to `find_best_packing_ordered` with the following functions:
+/// - `|l, r| l.area().cmp(&r.area())`
+/// - `|l, r| l.perimeter().cmp(&r.perimeter())`
+/// - `|l, r| l.w.max(l.h).cmp(&r.w.max(r.h))`
+/// - `|l, r| l.w.cmp(&r.w)`
+/// - `|l, r| l.h.cmp(&r.h)`
 pub fn find_best_packing<
     EmptySpacesType: EmptySpacesProviderTrait,
     T,
@@ -41,22 +68,57 @@ pub fn find_best_packing<
 ) -> RectWH
 where
     for<'a> &'a mut T: Into<&'a mut RectXYWH>,
-    for<'a> &'a T: Into<&'a RectXYWH>,
 {
-    find_best_packing_impl(root, create_box(subjects), subjects.len(), input)
+    find_best_packing_ordered(
+        root,
+        subjects,
+        input,
+        [
+            |l, r| l.area().cmp(&r.area()),
+            |l, r| l.perimeter().cmp(&r.perimeter()),
+            |l, r| l.w.max(l.h).cmp(&r.w.max(r.h)),
+            |l, r| l.w.cmp(&r.w),
+            |l, r| l.h.cmp(&r.h),
+        ],
+    )
+}
+
+/// Finds the best packing for a set of rectangles.
+///
+/// * `root` - Auxiliary storage for the algorithm.
+/// * `subjects` - The rectangles to pack. Their `x` and `y` components are filled in.
+/// * `input` - Settings for the algorithm.
+/// * `orders` - Ordering functions to use.
+pub fn find_best_packing_ordered<
+    EmptySpacesType: EmptySpacesProviderTrait,
+    T,
+    F: Fn(RectXYWH) -> CallbackResult,
+    G: Fn(RectXYWH) -> CallbackResult,
+    const N: usize,
+>(
+    root: &mut EmptySpaces<EmptySpacesType>,
+    subjects: &mut [T],
+    input: &Input<F, G>,
+    orders: [fn(RectXYWH, RectXYWH) -> Ordering; N],
+) -> RectWH
+where
+    for<'a> &'a mut T: Into<&'a mut RectXYWH>,
+{
+    find_best_packing_impl(root, create_box(subjects, orders), subjects.len(), input)
 }
 
 /// Creates a `Box` containing all orders, contiguously.
 /// This should be a performance improvement over having
 /// several `Vec`s separately.
-fn create_box<T>(subjects: &mut [T]) -> Box<[*mut RectXYWH]>
+fn create_box<T, const N: usize>(
+    subjects: &mut [T],
+    orderers: [fn(RectXYWH, RectXYWH) -> Ordering; N],
+) -> Box<[*mut RectXYWH]>
 where
     for<'a> &'a mut T: Into<&'a mut RectXYWH>,
-    for<'a> &'a T: Into<&'a RectXYWH>,
 {
-    // Five orders in total.
     let l = subjects.len();
-    let mut orders = Box::<[*mut RectXYWH]>::new_uninit_slice(l * 5);
+    let mut orders = Box::<[*mut RectXYWH]>::new_uninit_slice(l * N);
 
     for (i, s) in subjects.iter_mut().enumerate() {
         let r: &mut RectXYWH = s.into();
@@ -67,25 +129,29 @@ where
     }
 
     let (src, tgt) = orders.split_at_mut(l);
-    for chunk in tgt.chunks_exact_mut(l) {
+    src.sort_by(unsafe { s(orderers[0]) });
+
+    for (chunk, o) in tgt
+        .chunks_exact_mut(l)
+        .zip(orderers.iter().skip(1).copied())
+    {
         chunk.copy_from_slice(src);
+        chunk.sort_by(unsafe { s(o) });
     }
-
-    // Shorthand function to
-    let f = |i: usize| (l * i)..(l * (i + 1));
-
-    orders[f(0)].sort_by(s(|l, r| l.area().cmp(&r.area())));
-    orders[f(1)].sort_by(s(|l, r| l.perimeter().cmp(&r.perimeter())));
-    orders[f(2)].sort_by(s(|l, r| l.w.max(l.h).cmp(&r.w.max(r.h))));
-    orders[f(3)].sort_by(s(|l, r| l.w.cmp(&r.w)));
-    orders[f(4)].sort_by(s(|l, r| l.h.cmp(&r.h)));
 
     unsafe { orders.assume_init() }
 }
 
-/// Sorter shorthand function.
-fn s<F: Fn(RectXYWH, RectXYWH) -> Ordering>(
-    func: F,
+/// Convenience function that converts a "user-facing" sort function
+/// to one that can be used with a slice of `MaybeUninit`. In other
+/// words, converts from `Fn(RectXYWH, RectXYWH) -> Ordering` to
+/// `Fn(&MaybeUninit<RectXYWH>, &MaybeUninit<RectXYWH>) -> Ordering`.
+///
+/// # Safety
+/// This function assumes that the `MaybeUninit` slice you're
+/// sorting with the returned function is fully initialized.
+unsafe fn s(
+    func: fn(RectXYWH, RectXYWH) -> Ordering,
 ) -> impl Fn(&MaybeUninit<*mut RectXYWH>, &MaybeUninit<*mut RectXYWH>) -> Ordering {
     move |lhs, rhs| func(unsafe { *lhs.assume_init() }, unsafe { *rhs.assume_init() }).reverse()
 }
